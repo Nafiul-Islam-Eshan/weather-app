5f49de142456f05c58239f1f86ebe0a0a277e598
Nafiul-Islam-Eshan <nafiulislameshan307@gmail.com>
2026-03-24 13:17:59 +0600

    rr2233

--- script.js
+++ script.js
@@ -9,7 +9,10 @@ const windSpeed = document.getElementById("windSpeed");
 const searchInput = document.getElementById("searchInput");
 const searchBtn = document.getElementById("searchBtn");
 const weatherIcon = document.getElementById("weatherIcon");
-// console.log(weatherIcon);
+const weather1 = document.getElementById("weather1");
+const weather2 = document.getElementById("weather2");
+const errorDiv = document.getElementById("error");
+// console.log(errorDiv);
 
 const loadData = (city) => {
   fetch(`${apiUrl}&q=${city}&appid=${apiKey}&units=metric`)
@@ -18,26 +21,36 @@ const loadData = (city) => {
 };
 
 displayData = (data) => {
-  console.log(data.weather[0].main); // clouds, haze, clear, drizzle, mist, rain, snow
+  // console.log(data); // clouds, haze, clear, drizzle, mist, rain, snow
   celsiusTemp.innerText = Math.round(data.main.temp);
   city.innerText = data.name;
   humanity.innerText = data.main.humidity;
   windSpeed.innerText = data.wind.speed;
 
-  if (data.weather[0].main === "Clouds")
-    weatherIcon.src = "./weather-app-img/clouds.png";
-  else if (data.weather[0].main === "Haze")
-    weatherIcon.src = "./weather-app-img/haze.png";
-  else if (data.weather[0].main === "Mist")
-    weatherIcon.src = "./weather-app-img/mist.png";
-  else if (data.weather[0].main === "Clear")
-    weatherIcon.src = "./weather-app-img/clear.png";
-  else if (data.weather[0].main === "Drizzle")
-    weatherIcon.src = "./weather-app-img/drizzle.png";
-  else if (data.weather[0].main === "Rain")
-    weatherIcon.src = "./weather-app-img/rain.png";
-  else if (data.weather[0].main === "Snow")
-    weatherIcon.src = "./weather-app-img/snow.png";
+  if (data.cod === 404) {
+    errorDiv.classList.remove("hidden");
+    weather1.classList.add("hidden");
+    weather2.classList.add("hidden");
+  } else {
+    if (data.weather[0].main === "Clouds")
+      weatherIcon.src = "./weather-app-img/clouds.png";
+    else if (data.weather[0].main === "Haze")
+      weatherIcon.src = "./weather-app-img/haze.png";
+    else if (data.weather[0].main === "Mist")
+      weatherIcon.src = "./weather-app-img/mist.png";
+    else if (data.weather[0].main === "Clear")
+      weatherIcon.src = "./weather-app-img/clear.png";
+    else if (data.weather[0].main === "Drizzle")
+      weatherIcon.src = "./weather-app-img/drizzle.png";
+    else if (data.weather[0].main === "Rain")
+      weatherIcon.src = "./weather-app-img/rain.png";
+    else if (data.weather[0].main === "Snow")
+      weatherIcon.src = "./weather-app-img/snow.png";
+
+    errorDiv.classList.add("hidden");
+    weather1.classList.remove("hidden");
+    weather2.classList.remove("hidden");
+  }
 };
 
 searchBtn.addEventListener("click", () => {
