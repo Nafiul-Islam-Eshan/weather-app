4a15f16f48b12474f4f6bb574dfba377db211c77
Nafiul-Islam-Eshan <nafiulislameshan307@gmail.com>
2026-03-24 07:04:34 +0600

    Search fuctionality enabled

--- script.js
+++ script.js
@@ -1,17 +1,31 @@
 //  https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}&units=metric
 const apiKey = "56e3efd0161bd4924715340dbf2cafe4";
-const apiUrl =
-  "https://api.openweathermap.org/data/2.5/weather?q=Dhaka";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?";
 
 const celsiusTemp = document.getElementById("celsiusTemp")
 const city = document.getElementById("city")
 const humanity = document.getElementById("humanity")
 const windSpeed = document.getElementById("windSpeed")
-console.log(windSpeed);
+const searchInput = document.getElementById("searchInput")
+const searchBtn = document.getElementById("searchBtn")
+// console.log(searchBtn);
 
-const loadData = () => {
-  fetch(`${apiUrl}&appid=${apiKey}&units=metric`)
+const loadData = (city) => {
+  fetch(`${apiUrl}&q=${city}&appid=${apiKey}&units=metric`)
     .then((res) => res.json())
-    .then((data) => console.log(data));
+    .then((data) => displayData(data));
 };
-loadData();
+
+displayData = (data) => {
+  // console.log(data);
+  celsiusTemp.innerText = Math.round(data.main.temp)
+  city.innerText = data.name
+  humanity.innerText =data.main.humidity
+  windSpeed.innerText = data.wind.speed
+}
+
+searchBtn.addEventListener("click" , () => {
+  loadData(searchInput.value.trim());
+})
+
+
